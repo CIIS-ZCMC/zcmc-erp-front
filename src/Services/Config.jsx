bb43ad357c4bc5e27717c659d58394816367aa87
dev-artlouise <dev.artlouises@gmail.com>
2025-03-26 14:54:21 +0800

    test

--- src/Services/Config.jsx
+++ src/Services/Config.jsx
@@ -1,21 +1,3 @@
-/**
- * System Name must be assign here
- */
-export const SYSTEM_NAME = "Operations and Procurement Planning Management System";
-
-
-/**
- * Umis SSO Signing Path
- *
- * Don't change this path as this is the main authentication of the umis and your system.
- */
-export const SSO_SIGNING_PATH = "signing-in";
-
-/**
- * Directory of user first sigin
- * Change according to your system needs
- */
-export const ROOT_PATH = "/dashboard";
 
 /**
  * URLs of different site
