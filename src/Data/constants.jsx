d96426a75e39bead223733b245bd66e20fa43bcd
dev-artlouise <dev.artlouises@gmail.com>
2025-03-26 14:48:37 +0800

    additional configs

---
+++ src/Data/constants.jsx
@@ -0,0 +1,24 @@
+/**
+ * System Name must be assign here
+ */
+export const SYSTEM_NAME = "Operations and Procurement Planning Management System";
+
+
+/**
+ * Umis SSO Signing Path
+ *
+ * Don't change this path as this is the main authentication of the umis and your system.
+ */
+export const SSO_SIGNING_PATH = "signing-in";
+
+/**
+ * Directory of user first sigin
+ * Change according to your system needs
+ */
+export const ROOT_PATH = "/dashboard";
+
+
+export const EPR_API_ENDPOINTS = {
+    //ERP END POINTS RELATED HERE
+}
+
