04fd73882313f0df4d7f7d087b600e70d0e817ac
Alyana Barretto <alyanabarretto.zcmc@gmail.com>
2025-03-28 15:05:58 +0800

    try code

---
+++ src/Components/Common/ContainerComponent.jsx
@@ -0,0 +1,86 @@
+import { Box, Divider, Sheet, Stack, styled, Typography } from "@mui/joy";
+import PropTypes from "prop-types";
+import { getStatusColorScheme } from "../../Utils/ColorScheme";
+
+ContainerComponent.propTypes = {
+  children: PropTypes.node, // Allow multiple children
+  title: PropTypes.string,
+  description: PropTypes.string,
+  scrollable: PropTypes.bool,
+  contentMaxHeight: PropTypes.string,
+  actions: PropTypes.node,
+  noPadding: PropTypes.bool,
+  comingSoon: PropTypes.bool,
+  chipLabel: PropTypes.string,
+  chipColor: PropTypes.string,
+};
+
+const CustomSheet = styled(Sheet)(({ theme }) => ({
+  backgroundColor: "white",
+  borderRadius: 12,
+  padding: theme.spacing(2.5),
+  boxShadow: theme.shadow.md,
+}));
+
+function ContainerComponent({
+  children,
+  title,
+  description,
+  scrollable,
+  contentMaxHeight,
+  actions,
+  noPadding,
+  comingSoon = false,
+  chipLabel,
+  chipColor,
+  ...props
+}) {
+  return (
+    <CustomSheet {...props}>
+      {title && (
+        <Stack gap={1.5} mb={2}>
+          <Stack
+            direction={"row"}
+            sx={{ alignItems: "center", justifyContent: "space-between" }}
+          >
+            <Stack spacign={comingSoon && 0.4}>
+              <Typography
+                fontWeight={600}
+                fontSize={{ sm: "sm", md: "md", lg: "lg" }}
+              >
+                {title}{" "}
+                {comingSoon && (
+                  <span style={{ marginLeft: 4 }}>
+                    <ChipComponent
+                      // color={"neutral"}
+                      // variant={"solid"}
+                      label={"Coming soon"}
+                      size="sm"
+                    />
+                  </span>
+                )}
+              </Typography>
+              <Typography level="body-xs" fontWeight={400}>
+                {description}{" "}
+              </Typography>
+            </Stack>
+
+            {actions}
+          </Stack>
+          <Divider sx={{ marginX: noPadding && -2.5 }} />
+        </Stack>
+      )}
+
+      <Box
+        sx={{
+          maxHeight: scrollable ? contentMaxHeight : "none", // Adjust based on scrollable prop
+          overflowY: scrollable && "auto", // Show overflow only if scrollable
+        }}
+      >
+        {children}
+      </Box>
+    </CustomSheet>
+  );
+}
+
+export default ContainerComponent;
--- src/Pages/PlanningOps/QualitativeEvaluation.jsx
+++ src/Pages/PlanningOps/QualitativeEvaluation.jsx
@@ -1,9 +1,73 @@
-import React from 'react'
+import React from "react";
+import SheetComponent from "../../Components/Common/SheetComponent";
+import { Table, Typography } from "@mui/joy";
+import ContainerComponent from "../../Components/Common/ContainerComponent";
 
-const QualitativeEvaluation = () => {
-    return (
-        <div>QualitativeEvaluation</div>
-    )
+function QualitativeEvaluation({ props }) {
+  return (
+    <div>
+      <ContainerComponent
+        title={"List of Objectives and Success Indicators"}
+        description={
+          "This is a subheading. It should add more context to the interaction."
+        }
+      >
+        <Table
+          aria-label="basic table"
+          variant="outlined"
+          borderAxis="bothBetween"
+          stripe="even"
+        >
+          <thead>
+            <tr>
+              <th style={{ width: "40%" }}>Dessert (100g serving)</th>
+              <th>Calories</th>
+              <th>Fat&nbsp;(g)</th>
+              <th>Carbs&nbsp;(g)</th>
+              <th>Protein&nbsp;(g)</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Frozen yoghurt</td>
+              <td>159</td>
+              <td>6</td>
+              <td>24</td>
+              <td>4</td>
+            </tr>
+            <tr>
+              <td>Ice cream sandwich</td>
+              <td>237</td>
+              <td>9</td>
+              <td>37</td>
+              <td>4.3</td>
+            </tr>
+            <tr>
+              <td>Eclair</td>
+              <td>262</td>
+              <td>16</td>
+              <td>24</td>
+              <td>6</td>
+            </tr>
+            <tr>
+              <td>Cupcake</td>
+              <td>305</td>
+              <td>3.7</td>
+              <td>67</td>
+              <td>4.3</td>
+            </tr>
+            <tr>
+              <td>Gingerbread</td>
+              <td>356</td>
+              <td>16</td>
+              <td>49</td>
+              <td>3.9</td>
+            </tr>
+          </tbody>
+        </Table>
+      </ContainerComponent>
+    </div>
+  );
 }
 
-export default QualitativeEvaluation
+export default QualitativeEvaluation;
