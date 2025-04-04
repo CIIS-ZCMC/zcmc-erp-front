027f144198d1d073c6ecc27a7651edce6c2a7493
Alyana Barretto <alyanabarretto.zcmc@gmail.com>
2025-04-04 16:00:54 +0800

    ppmp page

--- src/Pages/DeptHead/EditPPMP/EditPPMP.jsx
+++ src/Pages/DeptHead/EditPPMP/EditPPMP.jsx
@@ -1,10 +1,39 @@
 import React, { Fragment } from "react";
 import PageTitle from "../../../Components/Common/PageTitle";
+import ContainerComponent from "../../../Components/Common/ContainerComponent";
+import ButtonComponent from "../../../Components/Common/ButtonComponent";
+import { BiPlus } from "react-icons/bi";
+import { Stack } from "@mui/joy";
 
-function EditPPMP(props) {
+function EditPPMP({ props }) {
   return (
     <Fragment>
-      <PageTitle title={"Update PPMP"} />
+      <PageTitle
+        title={"Edit PPMP"}
+        description={"Edit you unit's Project Procurement Management Plan"}
+      />
+      <ContainerComponent
+        title={"List of items"}
+        description={
+          "This is a subheading. It should add more context to the interaction."
+        }
+        sx={{ mt: 3 }}
+        actions={
+          <Stack direction={"row"} spacing={1}>
+            <ButtonComponent
+              label={"Read comments"}
+              color="success"
+              variant={"outlined"}
+            />
+            <ButtonComponent
+              label={"Add resources"}
+              color="success"
+              variant={"outlined"}
+              endDecorator={<BiPlus />}
+            />
+          </Stack>
+        }
+      ></ContainerComponent>
     </Fragment>
   );
 }
