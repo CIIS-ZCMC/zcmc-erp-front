8265aa3e920592dd35fb34243b8512a029c4f4b2
dev-artlouise <dev.artlouises@gmail.com>
2025-05-27 09:19:02 +0800

    create-aop

--- src/Pages/DeptHead/AnnualOps/Summary.jsx
+++ src/Pages/DeptHead/AnnualOps/Summary.jsx
@@ -94,7 +94,7 @@ const Summary = (
                         label={'Open request'}
                         variant={'outlined'}
                         size={'sm'}
-                        onClick={() => navigate(`/aop-edit/${8}`, { state: { id: 8 } })}
+                        onClick={() => navigate(`/aop-edit/${aop_application_id}`, { state: { id: aop_application_id } })}
                         endDecorator={<ExternalLink size={16} />}
                     />
 
