109a7dc57c229eb2820e24c08485986e0a8125c7
dev-artlouise <dev.artlouises@gmail.com>
2025-06-09 10:08:48 +0800

    merged from kriz-development

--- src/Pages/DeptHead/AnnualOps/AnnualOps.jsx
+++ src/Pages/DeptHead/AnnualOps/AnnualOps.jsx
@@ -80,7 +80,7 @@ const AnnualOps = () => {
           </BoxComponent>
           :
           <>
-            {!aop_application_id ?
+            {!aop_application_id ? //to be fixed
               <BoxComponent
                 mt={3}
                 height={'83vh'}
