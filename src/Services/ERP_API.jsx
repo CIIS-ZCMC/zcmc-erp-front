ec384f49c9bcfb5b1f0bbca9a404faed8ff5630d
TjayAmit <tristanjayamit0813@gmail.com>
2026-04-07 14:30:56 +0800

    fix: redirect to staging environment

--- src/Services/ERP_API.jsx
+++ src/Services/ERP_API.jsx
@@ -5,7 +5,7 @@ const BASE_URL = {
   development_landing_page: "http://192.168.5.1:5170", // This will be the landing page
   production_landing_page: "https://zcmc.online", // This will be the production landing page url
   production: "https://api_name.zcmc.online/api/", // Change the sub domain name to your prefer name
-  staging: "https://staging-erp.zcmc.online/api/", // Change the sub domain name to your prefer name
+  staging: "https://staging-erpapi.zcmc.online/api/", // Change the sub domain name to your prefer name
   development: "http://192.168.36.163:8000/api", // You can change the port or ip here
   local: "http://localhost:8000/api",
 };
